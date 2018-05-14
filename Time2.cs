cd2908d725debc17870cf9188590fc1a18b27c8e
ncockcro <35208141+ncockcro@users.noreply.github.com>
2018-05-14 10:03:12 -0400

    Completed lab 5 & 6
    Full implementation of the time2 class which stores time as a single integer and has properties to get the hours, minutes, and seconds. This also has lab 6 in it which is to add exception handling using try-catch blocks.

---
+++ Time2.cs
@@ -0,0 +1,165 @@
+﻿/*
+ * Written by: Nicholas Cockcroft
+ * Date: February 15, 2018
+ * Course: .NET Environment
+ * Assignemnt: Lab 5 & 6
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5and6
+{
+    class Time2
+    {
+        //private int hour;    // 0 -23
+        //private int minute;  // 0-59
+        //private int second;  // 0-59
+        private int m_time = 0; // holds hours, minutes, and seconds
+        private int resetTime; // used for resetting the time if a new value is passed in
+
+        // Time1 constructor initializes instance variables to 
+        // zero to set default time to midnight
+        public Time2()
+        {
+            SetTime(0, 0, 0);
+        }
+
+        // Set new time value in 24-hour format. Perform validity
+        // checks on the data. Set invalid values to zero.
+        public void SetTime(
+           int hourValue, int minuteValue, int secondValue)
+        {
+            // Reset m_time when it is called.
+            m_time = 0;
+
+            // If statements checking to make sure invalid inputs are handled
+            // and throw an exception which will be caught when the method is finished
+
+            if (hourValue < 0 || hourValue >= 24)
+            {
+                hourValue = 0;
+                throw new Exception("Hour is an invalid number, exception thrown.");
+            }
+            else if (minuteValue < 0 || minuteValue >= 60)
+            {
+                minuteValue = 0;
+                throw new Exception("Minute is an invalid number, exception thrown.");
+            }
+            else if (secondValue < 0 || secondValue >= 60)
+            {
+                secondValue = 0;
+                throw new Exception("Second is an invalid number, exception thrown.");
+            }
+            // If we get passed all of the exception handling, then we can treat it as normal inputs
+            else
+            {
+                m_time += 3600 * hourValue;
+                m_time += 60 * minuteValue;
+                m_time += secondValue;
+            }
+
+        } // end method SetTime
+
+        // convert time to universal-time (24 hour) format string
+        public string ToUniversalString()
+        {
+
+            return String.Format(
+               "{0:D2}:{1:D2}:{2:D2}", m_time / 3600, (m_time % 3600) / 60, m_time % 60);
+        }
+
+        // convert time to standard-time (12 hour) format string
+        public string ToStandardString()
+        {
+            return String.Format("{0}:{1:D2}:{2:D2} {3}",
+               (((m_time / 3600) == 12 || (m_time / 3600) == 0) ? 12 : (m_time / 3600) % 12),
+               (m_time % 3600) / 60, m_time % 60, ((m_time / 3600) < 12 ? "AM" : "PM"));
+        }
+
+        // Hour propeerty
+        public int Hour
+        {
+            get
+            {
+                return m_time / 3600;
+            }
+            set
+            {
+                // If statement used resetting the time if there was a previous time set
+                if ((m_time / 3600) != 0)
+                {
+                    resetTime = m_time / 3600;
+                    m_time -= resetTime * 3600;
+                }
+                // Exception is thrown if the hour input is invalid
+                if (value < 0 || value >= 24)
+                {
+                    throw new Exception("Not a valid hour, exception thrown.");
+                }
+                else
+                {
+                    m_time += 3600 * value;
+                }
+            }
+        }
+        // Minute Property
+        public int Minute
+        {
+            get
+            {
+                return (m_time % 3600) / 60;
+            }
+            set
+            {
+                // If statement used resetting the time if there was a previous time set
+                if ((m_time % 3600) / 60 != 0)
+                {
+                    resetTime = (m_time % 3600) / 60;
+                    m_time -= resetTime * 60;
+                }
+                // Throws exception if minutes is not a valid input
+                if (value < 0 || value >= 60)
+                {
+                    throw new Exception("Not a valid minute, exception thrown.");
+                }
+                else
+                {
+                    m_time += 60 * value;
+                }
+            }
+        }
+
+        // Second property
+        public int Second
+        {
+            get
+            {
+                return m_time % 60;
+            }
+            set
+            {
+                // If statement used resetting the time if there was a previous time set
+                if ((m_time % 60) != 0)
+                {
+                    resetTime = m_time % 60;
+                    m_time -= resetTime;
+                }
+                // Throwing exception if seconds are not a normal time
+                if (value < 0 || value >= 60)
+                {
+                    throw new Exception("Not a valid second. exception thrown.");
+                }
+                else
+                {
+                    m_time += value;
+                }
+
+            }
+        }
+    }
+}
---
+++ Time2Test.cs
@@ -0,0 +1,94 @@
+﻿/*
+ * Written by: Nicholas Cockcroft
+ * Date: February 15, 2018
+ * Course: .NET Environment
+ * Assignemnt: Lab 5 & 6
+ * 
+ * Description: Create a time class, Time2, which keeps time as a single integer. 
+ * Time2 should supply the same functionality as Time1. Also supply properties to get
+ * and set the hours, minutes, and seconds. Also for lab 6, modify this class so that
+ * the class will throw an exception if an illegal time is specified. 
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5and6
+{
+    class Time2Test
+    {
+        static void Main(string[] args)
+        {
+            Time2 time = new Time2();  // calls Time1 constructor
+            string output;
+
+            // assign string representation of time to output
+            output = "Initial universal time is: " +
+               time.ToUniversalString() +
+               "\nInitial standard time is: " +
+               time.ToStandardString();
+
+            // attempt valid time settings
+            time.SetTime(14, 22, 18);
+
+            // append new string representations of time to output
+            output += "\n\nUniversal time after SetTime is: " +
+               time.ToUniversalString() +
+               "\nStandard time after SetTime is: " +
+               time.ToStandardString();
+
+
+            // ------- Testing code for the properties. ---------
+                time.SetTime(10, 45, 55);
+
+            // Testing the hour, minute, second properties for getting time
+            int hour1 = time.Hour;
+            int min1 = time.Minute;
+            int sec1 = time.Second;
+
+            // Showing the getting properties functionality
+            output += "\n\nTesting the properties of getting time: " +
+               "\nHour: " + hour1 +
+               "\nMinute: " + min1 +
+               "\nSecond: " + sec1;
+
+            // Testing the hour, minute, second properties for setting time
+            // along with a try-catch block to make sure no invalid settings were made
+            try
+            {
+                time.Hour = 12;
+                time.Minute = 23;
+                time.Second = 14;
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+            }
+
+            // Testing the setting of minute after 12:23:14 was the set time
+            time.Minute = 20;
+
+            // Showing the setting properties fuctionality
+            output += "\n\nTesting the properties of setting time: " +
+               "\nUniversal time: " + time.ToUniversalString() +
+               "\nStandard time: " + time.ToStandardString();
+
+
+            // Testing time with one more example to make sure it still outputs
+            // the correct time even with a previous time already being set
+            time.Hour = 14;
+            time.Minute = 54;
+            time.Second = 34;
+
+            output += "\n\nTesting time after already having it set: " +
+               "\nUniversal time: " + time.ToUniversalString() +
+               "\nStandard time: " + time.ToStandardString();
+
+            MessageBox.Show(output, "Testing Class Time1");
+        }
+    }
+}
